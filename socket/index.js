fb72eaeff79465d5d6134bc717b4e869f57edcb3
Menyalin Alexey <menyalin@gmail.com>
2021-07-06 10:44:34 +0300

    #

--- socket/index.js
+++ socket/index.js
@@ -6,7 +6,7 @@ import { Server } from 'socket.io'
 const io = new Server({})
 
 const options = {
-  cors: '*'
+  cors: ['http://localhost:8080']
 }
 const emitTo = (to, eventType, payload) => {
   io.to(to).emit(eventType, payload)
