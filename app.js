dda4e96f70d5c86df9c13b5dbdc5d46128e5c822
Menyalin Alexey <menyalin@gmail.com>
2021-10-09 20:59:08 +0300

    ...

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(
     '*'
   ])
 )
-app.use(logger('dev'))
+if (process.env.MODE === 'dev') app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
