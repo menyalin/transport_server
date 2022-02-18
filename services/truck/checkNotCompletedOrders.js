1508b15955abf9d90c286441d4646f092539cb2d
Menyalin Alexey <menyalin@gmail.com>
2022-02-18 08:12:18 +0300

    fix query

--- services/truck/checkNotCompletedOrders.js
+++ services/truck/checkNotCompletedOrders.js
@@ -5,9 +5,13 @@ export default async function ({ truckId }) {
   const data = await Order.findOne({
     isActive: true,
     'confirmedCrew.truck': truckId,
-    'state.status': { $ne: 'confirmed' }
+    'state.status': { $ne: 'completed' }
   }).lean()
   if (data)
-    throw new BadRequestError('у данного ТС имеются не выполненные рейсы')
+    throw new BadRequestError(
+      `у данного ТС имеются не закрытые рейсы, от ${new Date(
+        data.startPositionDate
+      ).toLocaleDateString()}`
+    )
   else return null
 }
