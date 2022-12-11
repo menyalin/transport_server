8cfaa487d406045e966106582328462d98ed878f
Menyalin Alexey <menyalin@gmail.com>
2022-12-11 08:32:46 +0300

    fix query

--- services/crew/isLastItem.js
+++ services/crew/isLastItem.js
@@ -3,32 +3,32 @@ import { Crew } from '../../models/index.js'
 export default async ({ crew }) => {
   const lastTransportItem = crew.transport.pop()
   const lastDriverItem = await Crew.findOne({
-    $not: { _id: crew._id },
+    _id: { $ne: crew._id },
     company: crew.company,
     driver: crew.driver._id,
-    'transport.startDate': { $gt: lastTransportItem.startDate }
+    'transport.startDate': { $gt: lastTransportItem.startDate },
   }).lean()
   const lastTruckItem = await Crew.findOne({
-    $not: { _id: crew._id },
+    _id: { $ne: crew._id },
     company: crew.company,
     transport: {
       $elemMatch: {
         truck: lastTransportItem.truck,
-        startDate: { $gt: lastTransportItem.startDate }
-      }
-    }
+        startDate: { $gt: lastTransportItem.startDate },
+      },
+    },
   }).lean()
   let lastTrailerItem = null
   if (lastTransportItem.trailer)
     lastTrailerItem = await Crew.findOne({
-      $not: { _id: crew._id },
+      _id: { $ne: crew._id },
       company: crew.company,
       transport: {
         $elemMatch: {
           truck: lastTransportItem.trailer,
-          startDate: { $gt: lastTransportItem.startDate }
-        }
-      }
+          startDate: { $gt: lastTransportItem.startDate },
+        },
+      },
     }).lean()
   return !lastDriverItem && !lastTruckItem && !lastTrailerItem
 }
