6022be7e5efc8764989fbac9c9e5b79347f1fc78
Menyalin Alexey <menyalin@gmail.com>
2023-06-05 08:41:45 +0300

    wip: #40

--- models/paymentInvoice.js
+++ models/paymentInvoice.js
@@ -1,19 +1,10 @@
-import { PAIMENT_INVOICE_STATUSES_ENUM } from '../constants/paymentInvoice.js'
 import pkg from 'mongoose'
-const { Schema, model, Types } = pkg
 
-const schema = new Schema(
-  {
-    company: { type: Types.ObjectId, ref: 'Company', required: true },
-    number: { type: String },
-    sendDate: Date,
-    client: { type: Types.ObjectId, ref: 'Partner', required: true },
-    agreement: { type: Types.ObjectId, ref: 'Agreement', required: true },
-    status: { type: String, enum: PAIMENT_INVOICE_STATUSES_ENUM },
-    isActive: { type: Boolean, default: true },
-    note: String,
-  },
-  { timestamps: true }
-)
+import { PaymentInvoiceDomain } from '../domain/paymentInvoice/paymentInvoice.js'
+const { Schema, model } = pkg
+
+const schema = new Schema(PaymentInvoiceDomain.getDbSchema(), {
+  timestamps: true,
+})
 
 export default model('PaymentInvoice', schema, 'paymentInvoices')
