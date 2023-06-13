708a8cefa36f26024b85232850c6cffc2ec18bb0
Menyalin Alexey <menyalin@gmail.com>
2023-06-13 13:37:19 +0300

    wip: refactor order value object

--- values/order/routeStats.js
+++ values/order/routeStats.js
@@ -1,28 +1,12 @@
-
 import { Route } from './route.js'
 
 export class RouteStats {
-  static getDbSchema() {
-    return {
-      countPoints: Number,
-      loadingTimesInMinutes: [Number],
-      unloadingTimesInMinutes: [Number],
-      tripTimesInMinutes: [Number],
-      totalDurationInMinutes: Number,
-    }
-  }
-
-  // TODO: Переписать с учетом того что Route это объект-значение
   constructor(route) {
     if (!route || !route instanceof Route)
       throw new Error('RouteStats : constructor : invalid route!')
-
     this.countPoints = route.countOfPoints
-
     this.loadingTimesInMinutes = route.loadingTimesInMinutes
-
     this.unloadingTimesInMinutes = route.unloadingTimesInMinutes
-
     this.tripTimesInMinutes = route.tripTimes('minutes')
     this.totalDurationInMinutes = route.totalDuration('minutes')
   }
