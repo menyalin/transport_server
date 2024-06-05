566df85f7375768b8f013497a6efac34fbe1aa73
Menyalin Alexey <menyalin@icloud.com>
2024-06-05 10:09:46 +0300

    add test

--- src/domain/tariffContract/types/tarriffs/tests/idleTimeTariff.spec.ts
+++ src/domain/tariffContract/types/tarriffs/tests/idleTimeTariff.spec.ts
@@ -289,4 +289,52 @@ describe('IdleTimeTariff: calculate for order', () => {
     expect(unloadingDowntime.price).toEqual(2000)
     expect(returnDowntime.price).toEqual(0)
   })
+
+  it('calculateForOrder: округление времени простоя до 1 часа по плановому времени прибытия', () => {
+    const agreement = createAgreement({
+      vatRate: 0,
+      calcWaitingByArrivalDateLoading: false,
+      calcWaitingByArrivalDateUnloading: false,
+      noWaitingPaymentForAreLateLoading: false,
+      noWaitingPaymentForAreLateUnloading: false,
+    })
+    const order: Order = createTestOrder({
+      analytics: createOrderAnalytics({}),
+      route: [
+        new RoutePoint({
+          type: 'loading',
+          address: '1',
+          isMainLoadingPoint: true,
+          plannedDate: new Date('2024-01-02T00:00:00.000Z'),
+          arrivalDate: new Date('2024-01-02T00:00:00.000Z'),
+          departureDate: new Date('2024-01-02T00:10:00.000Z'),
+        }),
+
+        new RoutePoint({
+          type: 'unloading',
+          address: '1',
+          arrivalDate: new Date('2024-01-03T00:00:00.000Z'),
+          departureDate: new Date('2024-01-03T23:29:01.000Z'),
+        }),
+      ],
+    })
+    const tariff = createTariff({
+      includeHours: 0,
+      roundingInterval: 'hour',
+      tariffBy: 'hour',
+      price: 10000,
+    })
+
+    tariff.setContractData({
+      withVat: false,
+      contractName: 'fake contract',
+      contractDate: new Date('2024-05-15'),
+    })
+    const [loadingDowntime, unloadingDowntime, returnDowntime] =
+      tariff.calculateForOrder(order, agreement)
+
+    expect(loadingDowntime.price).toEqual(0)
+    expect(unloadingDowntime.price).toEqual(230000)
+    expect(returnDowntime.price).toEqual(0)
+  })
 })
