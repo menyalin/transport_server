2c0a236b0c79f89c37d34dab634c19d19c6c350f
Alexey <menyalin@icloud.com>
2026-05-15 09:29:08 +0300

    fix lint error

--- src/utils/sanitizeFilename.ts
+++ src/utils/sanitizeFilename.ts
@@ -3,6 +3,7 @@ export function sanitizeFilename(filename: string): string {
   return filename
     .replace(/[\\/:*?"<>|]/g, '') // недопустимые символы Windows
     .replace(/\s+/g, '_') // пробелы → подчеркивание
+    // eslint-disable-next-line no-control-regex
     .replace(/[\x00-\x1f\x80-\x9f]/g, '') // управляющие символы
     .replace(/^\.+/, '') // точки в начале имени
     .replace(/\.+$/, '') // точки в конце имени
